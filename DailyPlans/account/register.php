a39addbab7883a04798c346144f79d7691149842
FilMer17 <58984349+FilMer17@users.noreply.github.com>
2021-11-29 18:52:17 +0100

    To clone purpose

---
+++ DailyPlans/account/register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DailyPlans - Registrace</title>
+
+    <link rel="stylesheet" href="../css/formpage.css">
+</head>
+<body>
+    <form action="" class="form">
+        <h1 class="form__title">Vítej v DailyPlans</h1>
+        <h3 class="form__subtitle">Neboj a připoj se!</h3>
+        
+        <div class="input-container">
+            <input type="text" name="username" class="input" placeholder=" ">
+            <div class="label-box"></div>
+            <label for="username" class="input__label">Přihlašovací jméno</label>
+        </div>
+
+        <div class="input-container">
+            <input type="email" name="email" class="input" placeholder=" ">
+            <div class="label-box lb-short"></div>
+            <label for="email" class="input__label">Email</label>
+        </div>
+
+        <div class="input-container">
+            <input type="password" name="password" class="input" placeholder=" ">
+            <div class="label-box lb-short"></div>
+            <label for="password" class="input__label">Heslo</label>
+        </div>
+
+        <div class="input-container">
+            <input type="password" name="checkpass" class="input" placeholder=" ">
+            <div class="label-box lb-short"></div>
+            <label for="checkpass" class="input__label">Znovu</label>
+        </div>
+        
+        <button type="submit">Registrovat</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DailyPlans/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="dark light">
+    <title>Daily Plans - Plány na lepší dny</title> <!-- Plan for better days -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/mainpage.css">
+</head>
+<body>
+    <section class="head">
+        <header class="header">
+            <a href="" class="header__logo">DailyPlans</a>
+            <nav class="header__links">
+                <a href="account/signin.php" class="button">Přihlášení</a>
+                <a href="account/register.php" class="button">Registrace</a>
+                <a href="#" class="button">Vyzkoušet</a>
+            </nav>
+        </header>
+
+        <div class="head__title">
+            <h1>Daily Plans</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        </div>
+
+        <nav class="head__links">
+            <a href="#" class="button">Aspect1</a>
+            <a href="#" class="button">Aspect2</a>
+            <a href="#" class="button">Aspect3</a>
+        </nav>
+    </section>
+
+    <main class="main">
+        <article class="article">
+            <div class="article__side">
+                <h3>Aspect1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <img src="https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg" alt="Default image without content">
+        </article>
+        <hr />
+        <article class="article article-odd">
+            <div class="article__side">
+                <h3>Aspect1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <img src="https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg" alt="Default image without content">
+        </article>
+        <hr />
+        <article class="article">
+            <div class="article__side">
+                <h3>Aspect1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <img src="https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg" alt="Default image without content">
+        </article>
+    </main>
+    
+    <footer class="footer"></footer>
+</body>
+</html>
